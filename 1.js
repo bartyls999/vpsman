f1494b2f7e9af4de621a1094a397aa64e0c7d51b
bartyls999 <81957633+bartyls999@users.noreply.github.com>
2021-05-11 11:42:58 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Apache!\n');
-}).listen(8000, '34.83.255.38');
+}).listen(8000, '127.0.0.1');
